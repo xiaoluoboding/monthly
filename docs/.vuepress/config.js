a996e95b7cf04562f792048fb3b5523f359233de
xiaoluoboding <xiaoluoboding@gmail.com>
2019-12-26 17:21:54 +0800

    docs: init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: '前端技术栈月刊',
+  description: '聚焦前端，记录过去一个月发现的值得推荐的前端技术栈、文章以及应用，每月28日更新。',
+  themeConfig: {
+    nav: [
+      {
+        text: '2019', link: '/2019/',
+      },
+      {
+        text: '2020', link: '/2020/',
+      },
+      {
+        text: 'GitHub', link: 'https://github.com/xiaoluoboding/monthly'
+      }
+    ],
+    sidebar: [
+      {
+        title: '2019 年度',
+        path: '/2019 /',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: Array(12).fill().map((v, i) => {
+          let month = i + 1
+          let monthNum = month < 10 ? String(month).padStart(2, '0') : month
+          return `/2019/2019-${monthNum}`
+        })
+      },
+      {
+        title: '2020 年度',
+        path: '/2020/',
+        collapsable: false,
+        sidebarDepth: 2
+        // children: Array(1).fill().map((v, i) => {
+        //   let month = i + 1
+        //   let monthNum = month < 10 ? String(month).padStart(2, '0') : month
+        //   return `/2020/2020-${monthNum}`
+        // })
+      },
+    ]
+  },
+  plugins: ['@vuepress/back-to-top']
+}
---
+++ docs/README.md
@@ -0,0 +1,20 @@
+---
+home: true
+# heroImage: /element-schema-form__logo.jpg
+actionText: 开始阅读 →
+actionLink: /2019/2019-12/
+features:
+- title: 星推
+  details: 值得 Star 的 GitHub 仓库
+- title: 工具
+  details: 技术栈积累
+- title: 资源
+  details: 免费资源
+- title: 教程
+  details: 技术好文
+- title: 阅读
+  details: 有趣的文章
+- title: 应用
+  details: 精选macOS/iOS应用
+footer: MIT Licensed | Copyright © 2019-present xiaoluoboding
+---
