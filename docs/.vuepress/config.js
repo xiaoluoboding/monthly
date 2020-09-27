3d3d32aef43a79c994705c23c3722aba54731b12
xiaoluoboding <xiaoluoboding@gmail.com>
2020-09-27 23:59:42 +0800

    docs: update 202009

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
         text: '2019', link: '/2019/',
       },
       {
-        text: '2020', link: '/2020/2020-08',
+        text: '2020', link: '/2020/2020-09',
       },
       {
         text: '🗡️ Knives Out', link: '/knivesout/',
@@ -43,7 +43,7 @@ module.exports = {
         path: '/2020/',
         collapsable: false,
         sidebarDepth: 2,
-        children: getSidebar(2020, 8)
+        children: getSidebar(2020, 9)
       },
       {
         title: 'Knives Out',
