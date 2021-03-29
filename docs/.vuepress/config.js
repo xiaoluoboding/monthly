a207ca95b1dc5d5839f30e575615c4734dbc7c7c
xiaoluoboding <xiaoluoboding@gmail.com>
2021-03-30 00:12:46 +0800

    docs: update 2021/03

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
         text: '2020', link: '/2020/',
       },
       {
-        text: '2021', link: '/2021/2021-02',
+        text: '2021', link: '/2021/2021-03',
       },
       {
         text: '🗡️ Knives Out', link: '/knivesout/',
@@ -52,7 +52,7 @@ module.exports = {
         path: '/2021/',
         collapsable: false,
         sidebarDepth: 2,
-        children: getSidebar(2021, 02)
+        children: getSidebar(2021, 03)
       },
       {
         title: 'Knives Out',
